644ceb67ed966459a7e1952232b56548b236c6c6
Mell Basham <mellbasham@gmail.com>
2020-12-14 16:24:22 -0700

    commit tree 5667b2c7bc50b544c3aab4c106c9d90042222b5e

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+
+    def bark
+        puts "Woof!"
+    end
+    
+    def sit
+        puts "The Dog is sitting"
+    end
+end
+
+frank =Dog.new
+oboe = Dog.new
+boujbouj = Dog.new
+miranda = Dog.new
+
+
